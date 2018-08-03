ad82f2f434d551c51f694689d009aded97c10c23
Julian Fernandez <julian.ismael.fernandez@gmail.com>
2018-08-03 14:15:48 +0200

    Uploaded test sketch

---
+++ Code/Ground Station/Receiveworking.ino
@@ -0,0 +1,82 @@
+/*
+WORKING RECIEVER
+*/
+
+#include <LoRaLib.h>
+
+
+// NSS pin:   7
+// DIO0 pin:  2
+// DIO1 pin:  3
+SX1278 lora = new LoRa;
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize SX1278 with default settings
+  Serial.print(F("Initializing ... "));
+  // carrier frequency:           434.0 MHz
+  // bandwidth:                   125.0 kHz
+  // spreading factor:            9
+  // coding rate:                 7
+  // sync word:                   0x12
+  // output power:                17 dBm
+  // current limit:               100 mA
+  // preamble length:             8 symbols
+  // amplifier gain:              0 (automatic gain control)
+  int state = lora.begin(437.0, 125.0, 7, 8, 0x13);
+  if (state == ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true);
+  }
+}
+
+void loop() {
+  Serial.print("Waiting for incoming transmission ... ");
+
+  // you can receive data as an Arduino String
+  String str;
+  int state = lora.receive(str);
+
+  
+
+  if (state == ERR_NONE) {
+    // packet was successfully received
+    Serial.println("success!");
+
+    // print data of the packet
+    Serial.print("Data:\t\t\t");
+    Serial.println(str);
+
+    // print RSSI (Received Signal Strength Indicator) 
+    // of the last received packet
+    Serial.print("RSSI:\t\t\t");
+    Serial.print(lora.lastPacketRSSI);
+    Serial.println(" dBm");
+
+    // print SNR (Signal-to-Noise Ratio) 
+    // of the last received packet
+    Serial.print("SNR:\t\t\t");
+    Serial.print(lora.lastPacketSNR);
+    Serial.println(" dBm");
+
+    // print frequency error
+    // of the last received packet
+    Serial.print("Frequency error:\t");
+    Serial.print(lora.getFrequencyError());
+    Serial.println(" Hz");
+
+  } else if (state == ERR_RX_TIMEOUT) {
+    // timeout occurred while waiting for a packet
+    Serial.println("timeout!");
+
+  
+
+  }
+
+}
+
+
