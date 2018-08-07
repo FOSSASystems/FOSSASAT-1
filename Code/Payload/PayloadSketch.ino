eebf3fc157853cb25ba3fc26490865f79f9f89ad
Richard Bamford <richardbamford@outlook.com>
2018-08-07 17:51:53 +0100

    added communication relay for serial reading from payload. remove competition documents for now.

---
+++ Code/Payload/PayloadSketch.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write("6;");
+  delay(1000);
+}
