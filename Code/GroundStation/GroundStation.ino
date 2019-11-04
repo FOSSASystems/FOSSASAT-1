a5fddcae87572de8eca0c00fe4043d928db76fb7
jgromes <jgromes@users.noreply.github.com>
2019-11-04 22:11:45 +0100

    Added new ground station demo code

--- Code/GroundStation/GroundStation.ino
+++ Code/GroundStation/GroundStation.ino
@@ -1,255 +1,380 @@
 /*
  * FOSSA Ground Station Example
- * 
- * Setup is an arduino uno with a sx1278.
- * 
- * To use this program, plug it into your computer and send
- * commands via the arduino serial monitor.
- * 
- * Richard B.
- * 
- * 
+ *
+ * Tested on Arduino Uno and SX1278, can be used with any LoRa radio
+ * from the SX127x or SX126x series.
+ *
+ * Note: When using SX126x, make sure to use the correct sync word,
+ * set correct TCXO reference voltage and use DIO1 as the
+ * reception interrupt (since SX126x doesn't have DIO0)!
+ *
  * References:
- * 
- * radio.readData(...) Error code values - https://github.com/jgromes/RadioLib/blob/master/src/TypeDef.h
- * 
+ *
+ * RadioLib error codes:
+ * https://jgromes.github.io/RadioLib/group__status__codes.html
+ *
+ * FOSSASAT-1 Communication Guide:
+ * https://github.com/FOSSASystems/FOSSASAT-1/blob/master/FOSSA%20Documents/FOSSASAT-1%20Comms%20Guide.pdf
+ *
  */
 
+// include all libraries
 #include <RadioLib.h>
 #include <FOSSA-Comms.h>
 
-#define CS 10
-#define DIO0 2
-#define DIO1 6
+// pin definitions
+#define CS                10
+#define DIO0              2
+#define DIO1              3
 
+// modem configuration
+#define FREQUENCY         436.7   // MHz
+#define BANDWIDTH         125.0   // kHz
+#define SPREADING_FACTOR  11
+#define CODING_RATE       8       // 4/8
+#define SYNC_WORD_7X      0xFF    // sync word when using SX127x
+#define SYNC_WORD_6X      0x0F0F  //                      SX126x
+
+// set up radio module
 SX1278 radio = new Module(CS, DIO0, DIO1);
 
-volatile bool isInterruptEnabled = true;
-volatile bool isTransmissionReceived = false;
+// flags
+volatile bool interruptEnabled = true;
+volatile bool transmissionReceived = false;
 
+// satellite callsign
 char callsign[] = "FOSSASAT-1";
 
-/*
- * Utility functions
- */
+// radio ISR
+void onInterrupt() {
+  if(!interruptEnabled) {
+    return;
+  }
 
-void onInterrupt()
-{
-  if (!isInterruptEnabled) return;
-  
-  isTransmissionReceived = true;
+  transmissionReceived = true;
 }
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println(F("FOSSA Ground Station Initializing..."));
+// function to print controls
+void printControls() {
+  Serial.println(F("------------- Controls -------------"));
+  Serial.println(F("p - send ping frame"));
+  Serial.println(F("i - request satellite info"));
+  Serial.println(F("l - request last packet info"));
+  Serial.println(F("r - send message to be retransmitted"));
+  Serial.println(F("------------------------------------"));
+}
 
-  // initialize the radio.
-  int state = radio.begin(434.0, 125.0, 11, 8, 0x0F0F);
-  if (state == ERR_NONE)
-  {
-    Serial.println(F("Successfully initialized radio module!"));
-  }
-  else
-  {
-    Serial.print(F("Failed to initialize radio modue error code: "));
+void sendPing() {
+  Serial.print(F("Sending ping frame ... "));
+
+  // data to transmit
+  uint8_t functionId = CMD_PING;
+
+  // build frame
+  uint8_t len = FCP_Get_Frame_Length(callsign);
+  uint8_t* frame = new uint8_t[len];
+  FCP_Encode(frame, callsign, functionId);
+
+  // send data
+  int state = radio.transmit(frame, len);
+  delete[] frame;
+
+  // check transmission success
+  if (state == ERR_NONE) {
+    Serial.println(F("sent successfully!"));
+  } else {
+    Serial.print(F("failed, code "));
     Serial.println(state);
   }
-
-  // attach the ISR to this function
-  radio.setDio1Action(onInterrupt);
-  // begin listening for packets.
-  radio.startReceive();  
-
-  Serial.println(F("Completed."));
 }
 
-/**
- * This function takes a transmission frame and handles its data.
- */
-void ProcessReceivedTransmission(size_t* responsePacketLength, uint8_t* responsePacketFrame)
-{
-  // get the function id
-  uint8_t functionId = FCP_Get_FunctionID(callsign, responsePacketFrame, responsePacketLength);
+void requestInfo() {
+  Serial.print(F("Requesting system info ... "));
 
-  // get the packets length.
-  uint8_t responsePacketDataLength = FCP_Get_OptData_Length(callsign, responsePacketFrame, responsePacketLength);
+  // data to transmit
+  uint8_t functionId = CMD_TRANSMIT_SYSTEM_INFO;
 
+  // build frame
+  uint8_t len = FCP_Get_Frame_Length(callsign);
+  uint8_t* frame = new uint8_t[len];
+  FCP_Encode(frame, callsign, functionId);
 
-  // these variables store the optional data the packet has.
-  uint8_t* responsePacketData;
-  bool hasData = false;
-  
-  if (responsePacketDataLength > 0) // if we have data in packet.
-  { 
-    // extract the data to be used in the switch case later.
-    responsePacketData = new uint8_t[responsePacketDataLength];
-    
-    hasData = true;
-  } 
-
-  Serial.print(F("Received: "));
-  
-  switch (functionId)
-  {
-    case RESP_PONG:
-      Serial.println(F(" PONG"));
-      break;
-    case RESP_REPEATED_MESSAGE:
-      PRINT_BUFF(responsePacketData, responsePacketDataLength);
-      break;
-    case RESP_REPEATED_MESSAGE_CUSTOM:
-      PRINT_BUFF(responsePacketData, responsePacketDataLength);
-      break;
-    case RESP_SYSTEM_INFO:
-      PRINT_BUFF(responsePacketData, responsePacketDataLength);
-      break;
-    case RESP_LAST_PACKET_INFO:
-      PRINT_BUFF(responsePacketData, responsePacketDataLength);
-      break;
-  }
-
-  // make sure we free any memory allocated for the optional data.
-  if (hasData)
-  {
-    delete responsePacketData;
-  }
-}
-
-void SendTransmission(int serialDataLength)
-{
-  // extract the message from the serial input.
-  char* message = new char[serialDataLength];
-  
-  for (int i = 0; i < serialDataLength; i++)
-  {
-    message[i] = (char)(Serial.read());
-  }
-
-  Serial.print(F("Transmitting: "));
-  Serial.println(message);
-
-
-  // get the information for the frame.
-  char* data;
-  uint8_t functionId;
-  uint8_t dataLength;
-  bool includesData;
-
-  if (strcmp(message, "PING"))
-  {
-    functionId = CMD_PING;
-    includesData = false;
-  }
-  else if (strcmp(message, "GET_SYSTEM_INFO"))
-  {
-    functionId = CMD_TRANSMIT_SYSTEM_INFO;
-    includesData = false;
-  }
-  else if (strcmp(message, "GET_LAST_PACKET_INFO"))
-  {
-    functionId = CMD_GET_LAST_PACKET_INFO;
-    includesData = false;
-  }
-  else
-  {
-    functionId = CMD_RETRANSMIT;
-    includesData = true;
-    dataLength = strlen(message);
-    data = message;    
-  }
-
-
-  // construct the frame.
-  uint8_t frameLength;
-  if (includesData)
-  {
-    frameLength = FCP_Get_Frame_Length(callsign, dataLength, (uint8_t*)data);
-  }
-  else
-  {
-    frameLength = FCP_Get_Frame_Length(callsign);
-  }
-  
-  uint8_t* frame = new uint8_t[frameLength];
-  if (includesData)
-  {
-    FCP_Encode(frame, callsign, functionId, data, (uint8_t)data); 
-  }
-  else
-  {
-    FCP_Encode(frame, callsign, functionId);
-  }
-
-
-  // transmit the frame.
-  int state = radio.transmit(frame, frameLength);
-  if (state == ERR_NONE)
-  {
-    Serial.println(F("Successfully transmitted!"));
-  }
-  else
-  {
-    Serial.println(F("Error transmitting message..."));
-  }
-
+  // send data
+  int state = radio.transmit(frame, len);
   delete[] frame;
-  delete[] message;
+
+  // check transmission success
+  if (state == ERR_NONE) {
+    Serial.println(F("sent successfully!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
 }
 
-void loop()
-{
+void requestPacketInfo() {
+  Serial.print(F("Requesting last packet info ... "));
 
-  
-  // if we receive a serial message.
-  if (Serial.available() > 0)
-  {
-    // prevent receive interrupt.
-    isInterruptEnabled = false;
-    detachInterrupt(digitalPinToInterrupt(DIO1));
+  // data to transmit
+  uint8_t functionId = CMD_GET_LAST_PACKET_INFO;
 
-    // send a command.
-    SendTransmission(Serial.available());
+  // build frame
+  uint8_t len = FCP_Get_Frame_Length(callsign);
+  uint8_t* frame = new uint8_t[len];
+  FCP_Encode(frame, callsign, functionId);
 
-    // re-enable radio into receive mode.
-    radio.setDio1Action(onInterrupt);
-    isInterruptEnabled = true;
+  // send data
+  int state = radio.transmit(frame, len);
+  delete[] frame;
+
+  // check transmission success
+  if (state == ERR_NONE) {
+    Serial.println(F("sent successfully!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
+}
+
+void requestRetransmit() {
+  Serial.println(F("Enter message to be sent:"));
+  Serial.println(F("(max 32 characters, end with LF or CR+LF)"));
+
+  // get data to be retransmited
+  char optData[32];
+  uint8_t bufferPos = 0;
+  while(bufferPos < 32) {
+    while(!Serial.available());
+    char c = Serial.read();
+    Serial.print(c);
+    if((c != '\r') && (c != '\n')) {
+      optData[bufferPos] = c;
+      bufferPos++;
+    } else {
+      break;
+    }
   }
 
+  // wait for a bit to receive any trailing characters
+  delay(100);
 
-  
-  
-  // if we receive a transmission.
-  if (isTransmissionReceived)
-  {
-    // stop transmissions from being received.
-    isInterruptEnabled = false;
+  // dump the serial buffer
+  while(Serial.available()) {
+    Serial.read();
+  }
 
-    // get frame info and data.
-    size_t responsePacketLength = radio.getPacketLength();
-    uint8_t* responsePacketFrame = new uint8_t[responsePacketLength];
+  Serial.println();
+  Serial.print(F("Requesting retransmission ... "));
 
-    // process the frame.
-    int state = radio.readData(responsePacketFrame, responsePacketLength);
-    if (state != ERR_NONE)
-    {
-      // error
+  // data to transmit
+  uint8_t functionId = CMD_RETRANSMIT;
+  optData[bufferPos] = '\0';
+  uint8_t optDataLen = strlen(optData);
+
+  // build frame
+  uint8_t len = FCP_Get_Frame_Length(callsign, optDataLen);
+  uint8_t* frame = new uint8_t[len];
+  FCP_Encode(frame, callsign, functionId, optDataLen, (uint8_t*)optData);
+
+  // send data
+  int state = radio.transmit(frame, len);
+  delete[] frame;
+
+  // check transmission success
+  if (state == ERR_NONE) {
+    Serial.println(F("sent successfully!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("FOSSA Ground Station Demo Code"));
+
+  // initialize the radio
+  int state = radio.begin(FREQUENCY,
+                          BANDWIDTH,
+                          SPREADING_FACTOR,
+                          CODING_RATE,
+                          SYNC_WORD_7X);
+  if(state == ERR_NONE) {
+    Serial.println(F("Radio initialization successful!"));
+  } else {
+    Serial.print(F("Failed to initialize radio, code: "));
+    Serial.println(state);
+    while(true);
+  }
+
+  // attach the ISR to radio interrupt
+  radio.setDio0Action(onInterrupt);
+
+  // begin listening for packets
+  radio.startReceive();
+
+  printControls();
+}
+
+void loop() {
+  // check serial data
+  if(Serial.available()) {
+    // disable reception interrupt
+    interruptEnabled = false;
+    detachInterrupt(digitalPinToInterrupt(DIO0));
+
+    // get the first character
+    char serialCmd = Serial.read();
+
+    // wait for a bit to receive any trailing characters
+    delay(50);
+
+    // dump the serial buffer
+    while(Serial.available()) {
+      Serial.read();
+    }
+
+    // process serial command
+    switch(serialCmd) {
+      case 'p':
+        sendPing();
+        break;
+      case 'i':
+        requestInfo();
+        break;
+      case 'l':
+        requestPacketInfo();
+        break;
+      case 'r':
+        requestRetransmit();
+        break;
+      default:
+        Serial.print(F("Unknown command: "));
+        Serial.println(serialCmd);
+        break;
+    }
+
+    // set radio mode to reception
+    radio.setDio0Action(onInterrupt);
+    radio.startReceive();
+    interruptEnabled = true;
+  }
+
+  // check if new data were received
+  if(transmissionReceived) {
+    // disable reception interrupt
+    interruptEnabled = false;
+    transmissionReceived = false;
+
+    // read received data
+    size_t respLen = radio.getPacketLength();
+    uint8_t* respFrame = new uint8_t[respLen];
+    int state = radio.readData(respFrame, respLen);
+
+    // check reception success
+    if (state == ERR_NONE) {
+      // print raw data
+      Serial.print(F("Received "));
+      Serial.print(respLen);
+      Serial.println(F(" bytes:"));
+      PRINT_BUFF(respFrame, respLen);
+
+      // get function ID
+      uint8_t functionId = FCP_Get_FunctionID(callsign, respFrame, respLen);
+      Serial.print(F("Function ID: 0x"));
+      Serial.println(functionId, HEX);
+
+      // check optional data
+      uint8_t* respOptData = nullptr;
+      uint8_t respOptDataLen = FCP_Get_OptData_Length(callsign, respFrame, respLen);
+      Serial.print(F("Optional data ("));
+      Serial.print(respOptDataLen);
+      Serial.println(F(" bytes):"));
+      if(respOptDataLen > 0) {
+        // read optional data
+        respOptData = new uint8_t[respOptDataLen];
+        FCP_Get_OptData(callsign, respFrame, respLen, respOptData);
+        PRINT_BUFF(respFrame, respLen);
+      }
+
+      // process received frame
+      switch(functionId) {
+        case RESP_PONG:
+          Serial.println(F("Pong!"));
+          break;
+
+        case RESP_SYSTEM_INFO:
+          Serial.println(F("System info:"));
+
+          Serial.print(F("batteryChargingVoltage = "));
+          Serial.println(FCP_Get_Battery_Charging_Voltage(respOptData));
+
+          Serial.print(F("batteryChargingCurrent = "));
+          Serial.println(FCP_Get_Battery_Charging_Current(respOptData), 4);
+
+          Serial.print(F("batteryVoltage = "));
+          Serial.println(FCP_Get_Battery_Voltage(respOptData));
+
+          Serial.print(F("solarCellAVoltage = "));
+          Serial.println(FCP_Get_Solar_Cell_Voltage(0, respOptData));
+
+          Serial.print(F("solarCellBVoltage = "));
+          Serial.println(FCP_Get_Solar_Cell_Voltage(1, respOptData));
+
+          Serial.print(F("solarCellCVoltage = "));
+          Serial.println(FCP_Get_Solar_Cell_Voltage(2, respOptData));
+
+          Serial.print(F("batteryTemperature = "));
+          Serial.println(FCP_Get_Battery_Temperature(respOptData));
+
+          Serial.print(F("boardTemperature = "));
+          Serial.println(FCP_Get_Board_Temperature(respOptData));
+
+          Serial.print(F("mcuTemperature = "));
+          Serial.println(FCP_Get_MCU_Temperature(respOptData));
+
+          Serial.print(F("resetCounter = "));
+          Serial.println(FCP_Get_Reset_Counter(respOptData));
+
+          Serial.print(F("powerConfig = 0b"));
+          Serial.println(FCP_Get_Power_Configuration(respOptData), BIN);
+          break;
+
+        case RESP_LAST_PACKET_INFO:
+          Serial.println(F("Last packet info:"));
+
+          Serial.print(F("SNR = "));
+          Serial.print(respOptData[0] / 4.0);
+          Serial.println(F(" dB"));
+
+          Serial.print(F("RSSI = "));
+          Serial.print(respOptData[1] / -2.0);
+          Serial.println(F(" dBm"));
+          break;
+
+        case RESP_REPEATED_MESSAGE:
+          Serial.println(F("Got repeated message:"));
+          Serial.println((char*)respOptData);
+          break;
+
+        default:
+          Serial.println(F("Unknown function ID!"));
+          break;
+      }
+
+      if(respOptDataLen > 0) {
+        delete[] respOptData;
+      }
+
+    } else {
+      Serial.println(F("Reception failed, code "));
       Serial.println(state);
-    }
-    else
-    {
-      ProcessReceivedTransmission(responsePacketLength, responsePacketFrame);
+
     }
 
-
-    // free the memory.
-    delete[] responsePacketFrame;
-
-    // enable receiving transmissions again.
-    isInterruptEnabled = true;
+    // enable reception interrupt
+    delete[] respFrame;
+    radio.startReceive();
+    interruptEnabled = true;
   }
-
-
-  
 }
